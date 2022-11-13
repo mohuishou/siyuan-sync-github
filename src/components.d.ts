3d47eec726fcaa6dea785cf7a85a0ddee955ed4b
mohuishou <1@lailin.xyz>
2022-11-13 19:49:23 +0800

    ci: 支持自动发布到 release 分支

--- src/components.d.ts
+++ src/components.d.ts
@@ -13,8 +13,6 @@ declare module '@vue/runtime-core' {
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTag: typeof import('element-plus/es')['ElTag']
     Loading: typeof import('element-plus/es')['ElLoadingDirective']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
   }
 }
 
